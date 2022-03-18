8575773c4e5e177c55934ae14ccd9223ae2ffef8
odecaux <51046410+odecaux@users.noreply.github.com>
2022-03-18 11:54:38 +0100

    Compiler links with dsp.cpp

--- build_scripts/build_compiler.bat
+++ build_scripts/build_compiler.bat
@@ -3,6 +3,7 @@
 SETLOCAL
 
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL
+call "C:\Program Files (x86)\Intel\oneAPI\ipp\2021.5.0\env\vars.bat" intel64 >NUL
 
 set my_inc="C:\llvm\debug_msvc\include"
 set my_lib="C:\llvm\debug_msvc\lib"
@@ -13,7 +14,9 @@ set code=%cd%
 
 pushd build
 
-cl %debug_opts% /Fp"compiler.pch" /Yu"llvm_include.h" "%code%\compiler.cpp" /Fecompiler.dll  compiler_pch.obj   /D_USRDLL /D_WINDLL /link /LIBPATH:%my_lib% /DLL
+cl %debug_opts% /Fp"compiler.pch" /Yu"llvm_include.h" "%code%\compiler.cpp"   /D_USRDLL /c
+cl %debug_opts% "%code%\dsp.cpp" /c
+link compiler.obj dsp.obj compiler_pch.obj   ippcoremt.lib ippsmt.lib ippvmmt.lib Comdlg32.lib /OUT:compiler.dll /LIBPATH:%my_lib% /DLL
 
 popd
 
